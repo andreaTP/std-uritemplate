59366b3c41a925f15c4b6d68b5881725be9b2c22
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-03-11 11:52:58 +0000

    perform init

--- typescript-mjs/init.sh
+++ typescript-mjs/init.sh
@@ -1,1 +1,12 @@
-# no op
+#! /bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# This is intended to be a customizable entrypoint for each language, it has to be generic enough
+(
+  cd ${SCRIPT_DIR}/../typescript && \
+  rm -rf ${SCRIPT_DIR}/../typescript/dist && \
+  npm install && \
+  npm run clean && \
+  npm run build
+)
